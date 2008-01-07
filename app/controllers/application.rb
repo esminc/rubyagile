47e67a948c3eac5acd822c5e14adae9ea9ea6122
s-kakutani <s-kakutani@fcb0eba6-3158-4882-8cfe-26c7295f5ef0>
2008-01-07 17:05:12 +0000

    BASIC auth
    git-svn-id: svn+ssh://projects.tky.esm.co.jp/var/lib/svn/rubyagile/trunk@21 fcb0eba6-3158-4882-8cfe-26c7295f5ef0

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,7 +4,15 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
+  before_filter :basic_authenticate
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '935e963f099051ad538fff1f635764a1'
+
+  def basic_authenticate
+    authenticate_or_request_with_http_basic do |name, pass|
+      name == 'rubyxagile' && pass == 'yarvisawesome'
+    end
+  end
 end
