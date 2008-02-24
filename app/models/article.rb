72a8fd3ae4f6dff7d4a33bfaebcea2d2fd75dee1
s-kakutani <s-kakutani@fcb0eba6-3158-4882-8cfe-26c7295f5ef0>
2008-02-24 17:41:29 +0000

    add next/prev feature. add feed link on sidebar.
    git-svn-id: svn+ssh://projects.tky.esm.co.jp/var/lib/svn/rubyagile/trunk@65 fcb0eba6-3158-4882-8cfe-26c7295f5ef0

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,19 +4,21 @@ class Article < ActiveRecord::Base
   validates_presence_of :title, :body
 
   def prev_article
-    @prev_article ||= load_neighbors
+    load_neighbors unless @prev_article
+    (@prev_article == self ? nil : @prev_article)
   end
 
   def next_article
-    @next_article ||= load_neighbors
+    load_neighbors unless @next_article
+    (@next_article == self ? nil : @next_article)
   end
 
   private
   def load_neighbors
-    ids = (Article.find_by_sql [<<-SQL, id, id]).first
+    ids = (Article.find_by_sql [<<-SQL, {:id => id}]).first
 SELECT next.id as next_id, prev.id as prev_id FROM
- (select min(id) as id from articles where ? < id) next,
- (select max(id) as id from articles where id < ?) prev
+ (select min(id) as id from articles where :id < id) next,
+ (select max(id) as id from articles where id < :id) prev
     SQL
     @prev_article = Article.find_by_id(ids.prev_id)
     @next_article = Article.find_by_id(ids.next_id)
