34d42de2df74c630eaccf4ca8ab368d0acbcafea
s-kakutani <s-kakutani@fcb0eba6-3158-4882-8cfe-26c7295f5ef0>
2008-02-25 02:52:04 +0000

    add initial esm members
    git-svn-id: svn+ssh://projects.tky.esm.co.jp/var/lib/svn/rubyagile/trunk@71 fcb0eba6-3158-4882-8cfe-26c7295f5ef0

---
+++ db/migrate/006_load_users.rb
@@ -0,0 +1,11 @@
+require 'active_record/fixtures'
+class LoadUsers < ActiveRecord::Migration
+  def self.up
+    Fixtures.create_fixtures(File.dirname(__FILE__), File.basename("users.yml", ".*"))
+  end
+
+  def self.down
+    users = YAML.load_file("#{File.dirname(__FILE__)}/users.yml")
+    User.destroy(users.map{ |_, attr| attr["id"]}) rescue nil
+  end
+end
