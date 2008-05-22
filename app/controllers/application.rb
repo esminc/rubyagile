28b0d9acba7a47c6dd1222a0951c5e4d30970f64
s-kakutani <s-kakutani@fcb0eba6-3158-4882-8cfe-26c7295f5ef0>
2008-05-22 08:43:03 +0000

    change BASIC auth password.
    git-svn-id: svn+ssh://projects.tky.esm.co.jp/var/lib/svn/rubyagile/trunk@130 fcb0eba6-3158-4882-8cfe-26c7295f5ef0

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   private
   def authentication
     authenticate_or_request_with_http_basic do |user, pass|
-      user == 'oblove' && pass == 'oblove'
+      user == 'sp' && pass == 'rubyxagile'
     end
   end
 
