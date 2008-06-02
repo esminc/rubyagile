fd28c9243bb3d50efb721c3801a549a5f8587cae
KAKUTANI Shintaro <s-kakutani@esm.co.jp>
2008-06-02 13:55:14 +0900

    enable all spec passed.

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -7,25 +7,4 @@ describe ContactsController do
     controller.should be_an_instance_of(ContactsController)
   end
 
-
-  describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'confirm'" do
-    it "should be successful" do
-      get 'confirm'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'done'" do
-    it "should be successful" do
-      get 'done'
-      response.should be_success
-    end
-  end
 end
