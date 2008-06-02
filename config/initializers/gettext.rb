75412da27e50544b3c9a95c57db13d25ab83f3dd
KAKUTANI Shintaro <s-kakutani@esm.co.jp>
2008-06-03 02:15:55 +0900

    Adapted gettext to Rails 2.1.

--- config/initializers/gettext.rb
+++ config/initializers/gettext.rb
@@ -1,1 +1,9 @@
 require 'gettext/rails'
+
+# for 2.1
+# http://zargony.com/2008/02/12/edge-rails-and-gettext-undefined-method-file_exists-nomethoderror
+module ActionView
+  class Base
+    delegate :file_exists?, :to => :finder unless respond_to?(:file_exists?)
+  end
+end
