a5615fbe6a0e4ad636babbd3802e87f690d7a555
Keita Urashima <ursm@ursm.jp>
2009-04-25 20:36:56 +0900

    Change deploy settings for mary with Passenger.

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :runner, "rubyagile"
 ssh_options[:username] = application
 #ssh_options[:verbose] = :debug
 
-set :production_server, "ruby.agile.esm.co.jp"
+set :production_server, "agile.esm.co.jp"
 role :app, production_server
 role :web, production_server
 role :db,  production_server, :primary => true
@@ -26,23 +26,20 @@ role :db,  production_server, :primary => true
 set :rake, "/var/lib/gems/1.8/bin/rake"
 
 namespace :deploy do
-  desc "make .mo file"
-  task :makemo do
-    rake = fetch(:rake, "rake")
-    rails_env = fetch(:rails_env, "production")
-    run "cd #{latest_release}; #{rake} RAILS_ENV=#{rails_env} gettext:make:mo"
-  end
-
   task :after_update_code do
     src_db_yml = "#{shared_path}/config/database.yml"
     dest_db_yml = "#{latest_release}/config/database.yml"
     run "! test -e #{dest_db_yml} && ln -s #{src_db_yml} #{dest_db_yml}"
-#    makemo
   end
 
-  desc "resart"
+  %w(start stop).each do |t|
+    task t, :roles => :app do
+      # do nothing
+    end
+  end
+
+  desc "Restart application."
   task :restart, :roles => :app, :except => { :no_release => true } do |t|
-    stop
-    start
+    run "touch #{current_release}/tmp/restart.txt"
   end
 end
