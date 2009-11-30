5ee3a1ee305ddb97461cba9aa40bee7d44546388
Keita Urashima <ursm@ursm.jp>
2009-11-30 12:58:06 +0900

    おしらせのスタイルをまともにした

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @articles = Article.publishing.newer_first
+    @articles = Article.publishing.newer_first.all(:limit => 5)
     @entries = KarekiEntry.confirmed.newer_first.paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
