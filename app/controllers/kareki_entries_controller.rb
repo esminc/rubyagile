c58236508b2f475a3dde5cb39e8f6b0167fb3c29
Keita Urashima <ursm@ursm.jp>
2009-12-08 12:51:06 +0900

    KarekiEntry を confirm できるようにした

--- app/controllers/kareki_entries_controller.rb
+++ app/controllers/kareki_entries_controller.rb
@@ -4,4 +4,11 @@ class KarekiEntriesController < ApplicationController
   def index
     @entries = KarekiEntry.all
   end
+
+  def update
+    @entry = KarekiEntry.find(params[:id])
+    @entry.update_attributes(params[:kareki_entry])
+
+    redirect_to :action => :index
+  end
 end
