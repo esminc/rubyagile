6d3054f311f4551fc66feaf23274080d5d14cf70
Keita Urashima <ursm@ursm.jp>
2010-03-23 13:10:35 +0900

    [Fixed #2895568] サムネイル画像に直接リンクするのではなく、元のリンク先にリンクするようにした

--- app/helpers/kareki_entries_helper.rb
+++ app/helpers/kareki_entries_helper.rb
@@ -2,9 +2,8 @@ module KarekiEntriesHelper
 	def replace_img_to_anchor(html)
 		doc = Nokogiri::HTML::DocumentFragment.parse(html)
 
-		doc.css("a[href^='http://www.flickr.com/photos/']", 'a.hatena-fotolife').each do |e|
-			img = e.at('img')
-			e.replace Nokogiri::HTML::DocumentFragment.parse("<a href='#{img[:src]}'>#{img[:title]}</a>")
+		doc.css("a[href^='http://www.flickr.com/photos/']", 'a.hatena-fotolife').each do |a|
+			a.content = a.at('img')[:title]
 		end
 
 		doc.to_xhtml(:encoding => 'utf-8', :save_with => Nokogiri::XML::Node::SaveOptions::FORMAT)
