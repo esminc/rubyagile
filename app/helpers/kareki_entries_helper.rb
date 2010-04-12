08346f5df32a61a8df492460da7d20f545a7e0b7
moro <moronatural@gmail.com>
2010-04-12 14:11:03 +0900

    なかにimgタグを含まない画像リンクは触らない
    8ページ目の魔物問題を修正

--- app/helpers/kareki_entries_helper.rb
+++ app/helpers/kareki_entries_helper.rb
@@ -1,11 +1,11 @@
 module KarekiEntriesHelper
-	def replace_img_to_anchor(html)
-		doc = Nokogiri::HTML::DocumentFragment.parse(html)
+  def replace_img_to_anchor(html)
+    doc = Nokogiri::HTML::DocumentFragment.parse(html)
 
-		doc.css("a[href^='http://www.flickr.com/photos/']", 'a.hatena-fotolife').each do |a|
-			a.content = a.at('img')[:title]
-		end
+    doc.css("a[href^='http://www.flickr.com/photos/']", 'a.hatena-fotolife').
+      select {|a| a.at('img') }.
+      each { |a| a.content = a.at('img')[:title] }
 
-		doc.to_xhtml(:encoding => 'utf-8', :save_with => Nokogiri::XML::Node::SaveOptions::FORMAT)
-	end
+    doc.to_xhtml(:encoding => 'utf-8', :save_with => Nokogiri::XML::Node::SaveOptions::FORMAT)
+  end
 end
