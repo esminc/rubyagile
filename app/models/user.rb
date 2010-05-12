1888c076834abde6e2e35dbb451df89f4e7edfef
Keita Urashima <ursm@ursm.jp>
2010-05-12 13:45:55 +0900

    show only published/confirmed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :articles
   has_many :pages
   has_many :feeds, :class_name => KarekiFeed.name, :foreign_key => :owner_id
+  has_many :entries, :class_name => KarekiEntry.name, :through => :feeds
 
   validates_presence_of :login, :email, :open_id_url
 
