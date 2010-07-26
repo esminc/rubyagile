40397db9d45e0db97b9766c48e7d8ae4167511f4
Takumi Tsunokake <t-tsunokake@esm.co.jp>
2010-07-26 17:25:16 +0900

    [fixed #4273661] fixed links of entry.

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -2,7 +2,9 @@ Sham.define do
   login { Faker::Internet.user_name }
   email { Faker::Internet.email }
   open_id_url { "http://#{Faker::Internet.domain_name}" }
+  link { "http://#{Faker::Internet.domain_name}" }
   content { Faker::Lorem.sentence }
+  title { Faker::Lorem.sentence }
 end
 
 Page.blueprint do
@@ -13,4 +15,17 @@ User.blueprint do
   login
   email
   open_id_url
+end
+
+Article.blueprint do
+  title
+end
+
+KarekiEntry.blueprint do
+  title
+  creator { Sham.login }
+  link
+end
+
+KarekiFeed.blueprint do
 end
---
+++ spec/views/members/show.html.haml_spec.rb
@@ -0,0 +1,32 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "/members/show" do
+  before :each do
+    feed = KarekiFeed.make_unsaved
+    stub(feed).before_save
+    @entry = KarekiEntry.make(:feed => feed, :published_at => DateTime.now, :confirmation => "confirmed")
+    @article = Article.make(:publishing => true)
+    user = User.make(:articles => [@article], :feeds => [feed])
+    user.save!
+    assigns[:member] = user
+    render
+  end
+  
+  it "should have links of recent articles" do
+    response.should have_tag("ul#articles") do |ul|
+      ul.should have_tag("li") do |li|
+        li.should have_tag("a[href='#{article_path(@article)}']")
+        li.should have_tag("a", @article.title)
+      end
+    end
+  end
+
+  it "should have links of recent entries" do
+    response.should have_tag("ul#entries") do |ul|
+      ul.should have_tag("li") do |li|
+        li.should have_tag("a[href='#{@entry.link}']")
+        li.should have_tag("a", @entry.title)
+      end
+    end
+  end
+end
