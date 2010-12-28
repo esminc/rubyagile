5ca8426c2c4b46547a646f8edeb9a5bf151697b8
Keita Urashima <ursm@ursm.jp>
2010-12-28 19:44:37 +0900

    remove hoptoad_notifier

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,10 +69,4 @@ namespace :db do
   end
 end
 
-Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
-  $: << File.join(vendored_notifier, 'lib')
-end
-
-require 'hoptoad_notifier/capistrano'
-
 require 'bundler/capistrano'
