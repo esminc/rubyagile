b91f480c45447ed983b542b527e99970ca6c70a1
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-04-08 22:02:47 +0900

    oops

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rubyagile::Application.configure do
+RubyAgile::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
   # The production environment is meant for finished, "live" apps.
