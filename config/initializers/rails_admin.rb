f70870e9398364c326d31ca38aca254657096169
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-04-08 22:16:22 +0900

    use authenticate for current_user

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,7 +4,7 @@ RailsAdmin.current_user_method{ current_user }
 RailsAdmin.authenticate_with{ authenticate! }
 
 RailsAdmin.authorize_with {
-  redirect_to(:root, :alert => '権限がありません') unless authenticate?
+  redirect_to(:root, :alert => '権限がありません') unless current_user
 }
 
 RailsAdmin.config do |config|
