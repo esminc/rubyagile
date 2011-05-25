155b512541be5b8333332daf49f136de0363364d
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-05-25 22:09:04 +0900

    debug

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,15 +4,18 @@ class AuthenticationsController < ApplicationController
   end
 
   def create
+    raise request.env["omniauth.auth"].to_yaml
     auth = request.env["rack.auth"]
     current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
     flash[:notice] = "Authentication successful."
+    session[:user_id] = user.id
     redirect_to authentications_url
   end
 
   def destroy
     @authentication = current_user.authentications.find(params[:id])
     @authentication.destroy
+    session[:user_id] = nil
     flash[:notice] = "Successfully destroyed authentication."
     redirect_to authentications_url
   end
