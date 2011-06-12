94dc2e409784388cbc2c1f5d0ff17fb11a944fd4
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-06-12 10:24:17 +0900

    検索はオワコン

--- app/controllers/search_controller.rb
+++
@@ -1,7 +0,0 @@
-class SearchController < ApplicationController
-  def index
-    @q = params[:q]
-    @articles = @q.blank? ? Article.find(:all) : Article.find_fulltext(@q)
-    @pages = @q.blank? ? Page.find(:all) : Page.find_fulltext(@q)
-  end
-end
