62be2a153b2c91fc390bbaf2300fce01680c6c00
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-06-18 22:10:54 +0900

    pending

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], {:client_options => {:access_token_method => :get}}
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :linked_in, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
   provider :open_id, OpenID::Store::Filesystem.new('/tmp')
 end
