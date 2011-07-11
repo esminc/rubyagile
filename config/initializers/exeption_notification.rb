8d8b75718b2773790c94ffdab9a9b269942f0019
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-07-11 09:26:15 +0900

    tig/atig からのアクセスがActionView::MissingTemplateをraiseし続けるので notifier から除外した

--- config/initializers/exeption_notification.rb
+++ config/initializers/exeption_notification.rb
@@ -1,6 +1,8 @@
 if Rails.env.production?
+  # XXX tig/atig proxy access is raised to ActionView::MissingTemplate
   RubyAgile::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[rubyagile] ",
     :sender_address => %{"notifier" <notifier@example.com>},
-    :exception_recipients => %w{rubyagile@qwik.tky.esm.co.jp}
+    :exception_recipients => %w{rubyagile@qwik.tky.esm.co.jp},
+    :ignore_exceptions => %w{ActionView::MissingTemplate}
 end
