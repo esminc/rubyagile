cf49aecb6ed3e1d8d8320acc97e6704514236fc4
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-07-29 09:44:25 +0900

    fix deprecation worning

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,15 +1,12 @@
 # coding:utf-8
-RailsAdmin.current_user_method{ current_user }
-
-RailsAdmin.authenticate_with{ signed_in? }
-
-RailsAdmin.authorize_with {
-  unless current_user
-    flash[:error] = '権限がありません'
-    redirect_to '/'
-  end
-}
-
 RailsAdmin.config do |config|
-  config.list.default_items_per_page = 100
+  config.default_items_per_page = 100
+  config.current_user_method { current_user }
+  config.authenticate_with { signed_in? }
+  config.authorize_with {
+    unless current_user
+      flash[:error] = '権限がありません'
+      redirect_to '/'
+    end
+  }
 end
