57e093f96960857ad08a827a0d7ec2fe7d60e71a
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-08-11 13:19:06 +0900

    remove images relation.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,6 @@ class Article < ActiveRecord::Base
   paginates_per 5
 
   belongs_to :user
-  has_many :images, :dependent => :destroy
 
   default_scope :order => 'created_at DESC'
   scope :publishing, :conditions => {:publishing => true}
