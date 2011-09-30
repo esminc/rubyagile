d3bff35214e91a1ae9d36ae313e884c5f18495b4
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-09-30 10:47:47 +0900

    1.9.3 で syntax error

--- config/initializers/exeption_notification.rb
+++ config/initializers/exeption_notification.rb
@@ -1,6 +1,3 @@
 if Rails.env.production?
-  RubyAgile::Application.config.middleware.use ExceptionNotifier,
-    :email_prefix => "[rubyagile] ",
-    :sender_address => %{"notifier" <notifier@example.com>},
-    :exception_recipients => %w{rubyagile@qwik.tky.esm.co.jp},
+  RubyAgile::Application.config.middleware.use ExceptionNotifier, :email_prefix => "[rubyagile] ", :sender_address => %{"notifier" <notifier@example.com>}, :exception_recipients => %w{rubyagile@qwik.tky.esm.co.jp}
 end
