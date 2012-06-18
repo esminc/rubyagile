695bfea9f7b122fda043b6469201e65d2f5c3f8c
Mitsutaka Mimura <takkanm@gmail.com>
2012-06-18 11:18:33 +0900

    rails_adminのやつもprecompileするようにした

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ RubyAgile::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
